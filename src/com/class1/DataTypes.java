e784b7b9fa141d6f4e5edbb5ec53b5a6c7454bc9
selmatop <selmatop16@gmail.com>
2020-02-07 15:08:18 -0500

    java class added

---
+++ src/com/class1/DataTypes.java
@@ -0,0 +1,24 @@
+package com.class1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		short s=1234;
+		
+		boolean b=true;
+		boolean b1=false;
+		char c='a';
+		
+		String str="Hello";
+		
+		long l=123435657543l;
+		long l1=1234567890L;
+		float f=12.345f;
+		double d=123.3456789045678;
+		
+
+	}
+
+}
