dc7c8b902a67e036414cb70e82c449864358641f
selmatop <selmatop16@gmail.com>
2020-02-07 15:24:35 -0500

    new file

---
+++ src/com/class1/StringExample.java
@@ -0,0 +1,23 @@
+package com.class1;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="john";
+		String address="123 Test drive";
+		String salary="150.0000";
+		
+		System.out.println("Hello World");
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
